26d65b3ac5aba059e0d38c196a718213054d55a4
Joram Teusink <3344108+teusink@users.noreply.github.com>
2021-03-23 12:09:24 +0100

    Create fix-windows-update.bat

---
+++ files/fix-windows-update.bat
@@ -0,0 +1,47 @@
+net stop bits
+net stop wuauserv
+net stop appidsvc
+net stop cryptsvc
+Del “%ALLUSERSPROFILE%\Microsoft\Network\Downloader\qmgr*.dat
+cd /d %windir%\system32
+regsvr32.exe atl.dll
+regsvr32.exe urlmon.dll
+regsvr32.exe mshtml.dll
+regsvr32.exe shdocvw.dll
+regsvr32.exe browseui.dll
+regsvr32.exe jscript.dll
+regsvr32.exe vbscript.dll
+regsvr32.exe scrrun.dll
+regsvr32.exe msxml.dll
+regsvr32.exe msxml3.dll
+regsvr32.exe msxml6.dll
+regsvr32.exe actxprxy.dll
+regsvr32.exe softpub.dll
+regsvr32.exe wintrust.dll
+regsvr32.exe dssenh.dll
+regsvr32.exe rsaenh.dll
+regsvr32.exe gpkcsp.dll
+regsvr32.exe sccbase.dll
+regsvr32.exe slbcsp.dll
+regsvr32.exe cryptdlg.dll
+regsvr32.exe oleaut32.dll
+regsvr32.exe ole32.dll
+regsvr32.exe shell32.dll
+regsvr32.exe initpki.dll
+regsvr32.exe wuapi.dll
+regsvr32.exe wuaueng.dll
+regsvr32.exe wuaueng1.dll
+regsvr32.exe wucltui.dll
+regsvr32.exe wups.dll
+regsvr32.exe wups2.dll
+regsvr32.exe wuweb.dll
+regsvr32.exe qmgr.dll
+regsvr32.exe qmgrprxy.dll
+regsvr32.exe wucltux.dll
+regsvr32.exe muweb.dll
+regsvr32.exe wuwebv.dll
+netsh winsock reset
+net start bits
+net start wuauserv
+net start appidsvc
+net start cryptsvc
